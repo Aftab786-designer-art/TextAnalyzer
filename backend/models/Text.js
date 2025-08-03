01fafa1a6ccd8ecbe506b03fdb589f7b0b2f9308
Aftab Bashir <121763458+Aftab786-designer-art@users.noreply.github.com>
2025-08-03 12:11:36 +0500

    Removed backend from project

--- backend/models/Text.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const textSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('Text', textSchema);
